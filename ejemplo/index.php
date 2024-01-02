971f04b58ba865eb3215b708e2ef4ac8fc613bae
anamoyano6 <104173512+anamoyano6@users.noreply.github.com>
2024-01-02 20:14:03 -0300

    index

---
+++ ejemplo/index.php
@@ -0,0 +1,15 @@
+<?php 
+require_once("Config/config.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>POO</title>
+</head>
+<body>
+	<a href="<?php echo Config::$URL; ?>">HOME</a>
+	<a href="productos.php">Productos</a>
+</body>
+</html>
